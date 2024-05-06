1edbf57e6d7ecb0e5e0281cfeb85c22d8137a57b
zealotGuy <148281236+zealotGuy@users.noreply.github.com>
2024-05-05 21:55:55 -0700

    Create diagram.py
    diagram for hangman stress relief/mental health awareness game of sorts

---
+++ diagram.py
@@ -0,0 +1,59 @@
+stage_0 = r"""
+"""
+stage_1 =r"""
+  O
+"""
+stage_2 = r"""
+   O
+   |
+"""
+stage_3 = r"""
+   O
+  /|
+"""
+stage_4 = r"""
+   O
+  /|\
+"""
+stage_5 = r"""
+   O
+  /|\
+   |
+"""
+stage_6 = r"""
+   O
+  /|\
+ / | 
+"""
+stage_7 = r"""
+   O
+  /|\
+ / | \
+"""
+stage_8 = r"""
+   O
+  /|\
+ / | \
+  /
+"""
+stage_9 = r"""
+  O
+ /|\
+/ | \
+ / \ 
+"""
+stage_10 = r"""
+   O
+  /|\
+ / | \
+  / \
+ /
+"""
+stage_11 = r"""
+  O
+ /|\
+/ | \
+ / \
+/   \
+"""
+stages = [stage_0, stage_1, stage_2, stage_3, stage_4, stage_5, stage_6, stage_7, stage_8, stage_9, stage_10, stage_11]
