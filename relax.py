cbfadb4889ff7dff5ad15b742edf7ae63a59d942
Vijayesh314 <145930677+Vijayesh314@users.noreply.github.com>
2024-05-06 22:39:36 -0700

    Update relax.py

--- relax.py
+++ relax.py
@@ -1,57 +1,161 @@
-import requests
-from diagram import *  
+import random
+stage_0 = r"""
+   -----------  
+      |     |
+            |
+            |
+            |
+            |
+            |
+ --------------
+"""
+stage_1 =r"""
+   -----------  
+      |     |
+      0     |
+            |
+            |
+            |
+            |
+ --------------
+"""
+stage_2 = r"""
+   -----------  
+      |     |
+      0     |
+      |     |
+            |
+            |
+            |
+ --------------
+"""
+stage_3 = r"""
+  ------------  
+      |    |
+      0    |
+    / |    |
+           |
+           |
+           |
+---------------
+"""
+stage_4 = r"""
+   -----------  
+      |     |
+      0     |
+    / | \   |
+            |
+            |
+            |
+ --------------
+"""
+stage_5 = r"""
+   -----------  
+      |     |
+      0     |
+    / | \   |
+      |     |
+            |
+            |
+ --------------
+"""
+stage_6 = r"""
+   -----------  
+      |     |
+      0     |
+    / | \   |
+      |     |
+     /      |
+            |
+ --------------
+"""
+stage_7 = r"""
+   -----------  
+      |     |
+      0     |
+    / | \   |
+      |     |
+     / \    |
+            |
+ --------------
+"""
+stage_8 = r"""
+   -----------  
+      |     |
+      0     |
+    / | \   |
+   /  |  \  |
+     / \    |
+    /       |
+ --------------
+"""
+stage_9 = r"""
+   -----------  
+      |     |
+      0     |
+    / | \   |
+   /  |  \  |
+     / \    |
+    /   \   |
+ --------------
+"""
 
-def getWordFromAPI():
-  api = "https://random-word-api.herokuapp.com/word"
-  data = requests.get(api)
-  dictionary = data.json()
-  rand_word = dictionary[0]
-  return rand_word
+stages = [stage_0, stage_1, stage_2, stage_3, stage_4, stage_5, stage_6, stage_7, stage_8, stage_9]
+#The player starts with 0 tries
+lives = 0
 
-rand_word = getWordFromAPI()
-guessed_set = set()
-wrong_guess = 0
-total_wrong_guess = 0
+#Make a list of words that we want to use in the game. Each item in the list is a word with five characters
+words = ['anxiety', 'sympathy', 'depression', 'empathy', 'eustress', 'distress', 'suicide', 'dopamine', 'addiction', 'stigma']
 
-underscore = ["_" for i in range(len(rand_word))]
+#Pick a word from wordlist at random.
+secret_word = random.choice(words)
 
-def run_game():  
+#Tell the user what the game is - no special characters, only letters
+#Make another list with '?'. Every ? represents one letter-secretword
+clue = list('?' for i in range(len(secret_word)))
 
-  global wrong_guess
-  global total_wrong_guess  
-  
-  while "_" in underscore:
-    print("".join(underscore))
-    print(guessed_set)
-    guess = input("Guess a letter: ")
-    
-    if guess not in guessed_set:
-       guessed_set.add(guess)
-    
-    else:
-      print("Already guessed letter, try again.")
-      continue
-    
-    for x in range(len(rand_word)):
-      if rand_word[x]==guess:
-         underscore[x] = guess
-        
-      if rand_word[x] != guess:
-         wrong_guess += 1
-            
-    if guess not in rand_word:
-       total_wrong_guess += 1
-    
-       print(stages[total_wrong_guess])
-    if total_wrong_guess == len(stages):
-       print(stage_11)
-       print("Game over for you...")
-       exit()
-    
-    wrong_guess = 0
-    
-    if "_" not in underscore:
-       print(rand_word)
-       print("congratulations, you won!")
+heart_symbol = '\u2764\uFE0F'
+guessed_word_correctly = False
 
-run_game()
+incorrect_guesses = []
+
+def update_clue(guessed_letter, secret_word, clue):
+   #Guessed_letter = i, secret_word = pizza, clue = ?????
+   index = 0
+   while index < len(secret_word):
+      if guessed_letter == secret_word[index]:
+         clue[index] = guessed_letter
+      index = index + 1
+
+
+while lives <= 9:
+   print(clue)
+   print('Lives completed:', heart_symbol, lives)
+   print(stages[lives])
+   
+   #Ask the player to guess the letter
+   guess = input('guess a letter or the whole word: ')
+
+   if guess == secret_word:
+      #When the word is guessed correctly, this will break out of the loop
+      guessed_word_correctly = True
+      break
+
+   if guess in secret_word:
+      update_clue(guess, secret_word, clue)
+
+   else:
+      #check wheter guessletter is part of secretword, if no, subtract a life
+      print('Incorrect. You lost a life')
+      lives += 1
+      incorrect_guesses.append(guess)
+   
+   print(incorrect_guesses)
+
+
+#Tell the player if they are correct or not
+#continue this loop until either the word is guessed by player or all nine lives are lost
+if guessed_word_correctly:
+   print('You won! the secret word was ', secret_word)
+else:
+   print('You lost! The secret word was ', secret_word)
